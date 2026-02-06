3746b78645461b0e3fe85e84f5343d99a055cf05
Yannick Winkler <yannickwinkler3@gmail.com>
2026-02-06 14:32:40 +0100

    Added logger

---
+++ Backend/logger.cjs
@@ -0,0 +1,25 @@
+const winston = require("winston")
+
+const logger = winston.createLogger({
+    level: process.env.LOG_LEVEL || "info",
+    format: winston.format.combine(
+        winston.format.timestamp({format: "YYYY-MM-DD HH:mm:ss"}),
+        winston.format.printf(({timestamp, level, message, ...meta}) => {
+            const metaStr = Object.keys(meta).length
+            ? `${JSON.stringify(meta)}`
+            : "";
+            return `[${timestamp}] ${level.toUpperCase()}: ${message}${metaStr}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({filename: "server.log"})
+    ]
+});
+
+// Usage examples:
+// logger.info("Started server")
+// logger.warning("Invalid input recieved")
+// logger.error("Database could not be reached")
+
+module.exports = logger
