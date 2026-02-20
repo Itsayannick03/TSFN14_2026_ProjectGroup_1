b0b8c1da12017f20187518a5619efd214f880fb6
Fadde1 <jafa22my@student.ju.se>
2026-02-20 12:48:59 +0100

    created Terraform

---
+++ Frontend/terraform/main.tf
@@ -0,0 +1,63 @@
+resource "kubernetes_namespace" "app" {
+  metadata {
+    name = "hair-salon"
+  }
+}
+
+resource "kubernetes_deployment" "app" {
+  metadata {
+    name      = "hair-salon-deployment"
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "hair-salon"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "hair-salon"
+        }
+      }
+
+      spec {
+        container {
+          name  = "hair-salon-container"
+          image = "hair-salon-app:latest"
+
+          image_pull_policy = "Never"
+
+          port {
+            container_port = 5000
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "app" {
+  metadata {
+    name      = "hair-salon-service"
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "hair-salon"
+    }
+
+    port {
+      port        = 5000
+      target_port = 5000
+    }
+
+    type = "NodePort"
+  }
+}
