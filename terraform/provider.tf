5e7bd88e0094d9e6290bbd1efe4ec3e3f7bfd695
Fadde1 <jafa22my@student.ju.se>
2026-03-02 13:02:50 +0100

    added test for booking

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,10 @@
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
@@ -7,6 +12,14 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
+}
+
+# Kubernetes provider configured dynamically from AKS
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
 }
